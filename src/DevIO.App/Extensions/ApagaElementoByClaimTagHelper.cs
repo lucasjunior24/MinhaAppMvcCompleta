96fa8b7302fbb27a5538218377c73a30a9da4f1c
lucas souza <lukateku@gmail.com>
2021-08-05 17:40:49 -0400

    Ajustes na segurança

--- src/DevIO.App/Extensions/ApagaElementoByClaimTagHelper.cs
+++ src/DevIO.App/Extensions/ApagaElementoByClaimTagHelper.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Routing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +12,93 @@ namespace DevIO.App.Extensions
     [HtmlTargetElement("*", Attributes = "supress-by-claim-value")]
     public class ApagaElementoByClaimTagHelper : TagHelper
     {
+        private readonly IHttpContextAccessor _httpContextAcessor;
+
+        public ApagaElementoByClaimTagHelper(IHttpContextAccessor httpContextAcessor)
+        {
+            _httpContextAcessor = httpContextAcessor;
+        }
+
+        [HtmlAttributeName("supress-by-claim-name")]
+        public string IdentityClaimName { get; set; }
+        [HtmlAttributeName("supress-by-claim-value")]
+        public string IdentityClaimValue { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if(context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (output == null)
+                throw new ArgumentNullException(nameof(context));
+
+            var temAcesso = CustomAuthorization.ValidarClaimsUsuario(_httpContextAcessor.HttpContext, IdentityClaimName, IdentityClaimValue);
+
+            if (temAcesso) return;
+
+            output.SuppressOutput();
+        }
+    }
+    
+
+    [HtmlTargetElement("a", Attributes = "disable-by-claim-name")]
+    [HtmlTargetElement("a", Attributes = "disable-by-claim-value")]
+    public class DesabilitaElementoByClaimTagHelper : TagHelper
+    {
+        private readonly IHttpContextAccessor _contextAcessor;
+
+        public DesabilitaElementoByClaimTagHelper(IHttpContextAccessor contextAcessor)
+        {
+            _contextAcessor = contextAcessor;
+        }
+
+        [HtmlAttributeName("disable-by-claim-name")]
+        public string IdentityClaimName { get; set; }
+        [HtmlAttributeName("disable-by-claim-value")]
+        public string IdentityClaimValue { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (output == null)
+                throw new ArgumentNullException(nameof(context));
+
+            var temAcesso = CustomAuthorization.ValidarClaimsUsuario(_contextAcessor.HttpContext, IdentityClaimName, IdentityClaimValue);
+
+            if (temAcesso) return;
+
+            output.Attributes.RemoveAll("href");
+            output.Attributes.Add(new TagHelperAttribute("style", "cursor: not-allowed"));
+            output.Attributes.Add(new TagHelperAttribute("title", "Voçê nã tem permissão!"));
+        }
+    }
+
+
+    [HtmlTargetElement("*", Attributes = "supress-by-action")]
+    public class ApagaElementoByActionTagHelper : TagHelper
+    {
+        private readonly IHttpContextAccessor _contextAcessor;
+
+        public ApagaElementoByActionTagHelper(IHttpContextAccessor contextAcessor)
+        {
+            _contextAcessor = contextAcessor;
+        }
+
+        [HtmlAttributeName("supress-by-action")]
+        public string ActionName { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (output == null)
+                throw new ArgumentNullException(nameof(context));
+
+            var action = _contextAcessor.HttpContext.GetRouteData().Values["action"].ToString();
+
+            if (ActionName.Contains(action)) return;
+
+            output.SuppressOutput();
+        }
     }
 }
